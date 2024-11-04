7390109ce38cbd3dc721dda0ba6caf3fa25c124b
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-11-04 15:52:05 -0500

    fix: Caching failed for pack

--- app/(auth)/auth/confirm/route.ts
+++ app/(auth)/auth/confirm/route.ts
@@ -1,5 +1,6 @@
-import { type EmailOtpType } from '@supabase/supabase-js';
-import { type NextRequest, NextResponse } from 'next/server';
+import { EmailOtpType } from '@supabase/supabase-js';
+import { NextRequest } from 'next/server';
+import { redirect } from 'next/navigation';
 
 import { createClient } from '@/utils/supabase/server';
 
@@ -9,25 +10,21 @@ export async function GET(request: NextRequest) {
   const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/';
 
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete('token_hash');
-  redirectTo.searchParams.delete('type');
-
-  if (token_hash && type) {
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-    if (!error) {
-      redirectTo.searchParams.delete('next');
-      return NextResponse.redirect(redirectTo);
-    }
+  if (!token_hash || !type) {
+    redirect('/error');
   }
 
-  // return the user to an error page with some instructions
-  redirectTo.pathname = '/error';
-  return NextResponse.redirect(redirectTo);
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.verifyOtp({
+    type,
+    token_hash,
+  });
+
+  if (error) {
+    redirect('/error');
+  }
+
+  // If successful, redirect to the next page
+  redirect(next);
 }
