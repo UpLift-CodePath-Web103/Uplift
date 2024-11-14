753af1ee41efc09d4c4f0b5b7fe3595ffc1c8860
Aditya Jha <aditya.jha2020123@gmail.com>
2024-11-13 23:21:20 -0500

    Breathing excersises are done

--- app/(journal)/components/Video.tsx
+++ app/(journal)/components/Video.tsx
@@ -1,8 +1,16 @@
-export function Video(props: { src: string }) { 
-    return ( 
-        <div className="aspect-h-9 aspect-w-16"> 
-            <iframe className="rounded-lg" src={props.src} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /> 
-        </div> 
-        );
-    }
-
+export function Video(props: { src: string }) {
+    return (
+      <div className="w-full max-w-2xl mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
+        <div className="relative w-full h-0 overflow-hidden rounded-lg" style={{ paddingBottom: '56.25%' }}>
+          <iframe
+            className="absolute top-0 left-0 w-full h-full rounded-lg"
+            src={props.src}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    );
+  }
+  
