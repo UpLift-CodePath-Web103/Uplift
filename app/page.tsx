73b44710108cb51246009eae92ea6c584b96c041
Kazi Sadman <ksadman000@citymail.cuny.edu>
2024-11-21 02:16:03 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,15 @@ export default function Home() {
   return (
     <div
       className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]' // Set text color to white
-      style={{
-        backgroundImage: `url(${backgroundImage.src})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        position: 'relative', 
-      }}
     >
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
         {/* Increased text size */}
         <h1 className='text-7xl sm:text-8xl md:text-9xl font-semibold text-center sm:text-left z-10 text-white'>
           Uplift
         </h1>
-        
+        <Button><Link href={'/journal'}>Dashboard</Link></Button>
         <div className="absolute top-8 right-8 z-10 text-black space-x-5">
           <LoginLogoutButton />
-          <Button><Link href={'/journal'}>Dashboard</Link></Button>
-
         </div>
       </main>
     </div>
