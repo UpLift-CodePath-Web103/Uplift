7c036731fe277481bc5620d80a1d6687e138c4a2
Aditya Jha <aditya.jha2020123@gmail.com>
2024-11-21 11:15:21 -0500

    Fixed landing page, again...

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,12 @@ export default function Home() {
     >
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
         {/* Increased text size */}
-        <h1 className='text-7xl sm:text-8xl md:text-9xl font-semibold text-center sm:text-left z-10 text-white'>
+        <h1 className='text-7xl sm:text-8xl md:text-9xl font-semibold text-center sm:text-left z-10 text-black'>
           Uplift
         </h1>
+        <h2 className='text-5xl sm:text-6xl md:text-7xl font-semibold text-center sm:text-left z-10 text-black'>
+          <UserGreetText />
+        </h2>
         <Button><Link href={'/journal'}>Dashboard</Link></Button>
         <div className="absolute top-8 right-8 z-10 text-black space-x-5">
           <LoginLogoutButton />
